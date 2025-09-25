d26765b1e7f1c903c4d3d030c5613c7fbc6da636
priyobrato.das <priyodas12@gmail.com>
2025-09-25 12:30:40 +0530

    cards

--- src/components/restaurants/Restaurants.jsx
+++ src/components/restaurants/Restaurants.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import './Restaurants.css'
+import RestaurantCard from "./cards/RestaurantCard";
 
 
 
 function Restaurants(){
     return <div id="restaurants">
        <p>Restaurants cards</p>
+       <RestaurantCard></RestaurantCard>
+       <RestaurantCard></RestaurantCard>
+       <RestaurantCard></RestaurantCard>
+       <RestaurantCard></RestaurantCard>
     </div>
 }
 
---
+++ src/components/restaurants/cards/RestaurantCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './RestaurantCard.css';
+
+function RestaurantCard(){
+    return <div id='restaurant-card'>
+        <p>RestaurantCard</p>
+    </div>
+}
+
+export default RestaurantCard;
