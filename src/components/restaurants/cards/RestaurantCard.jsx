899e063659458822a84e21256e1bcf695326c220
priyobrato.das <priyodas12@gmail.com>
2025-09-25 13:49:08 +0530

    added bg: image

--- src/components/restaurants/cards/RestaurantCard.jsx
+++ src/components/restaurants/cards/RestaurantCard.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import './RestaurantCard.css';
+import uuid4 from "uuid4";
+import image1 from "../../../public/images/1.jpg"
 
 function RestaurantCard(){
-    return <div id='restaurant-card'>
-        <p>RestaurantCard</p>
+    let restId=uuid4();
+    return <div id='restaurant-card' style={{ "--card-background-image": `url(${image1})`}} >
+        <p>{restId}</p>
     </div>
 }
 
