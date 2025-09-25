c249f9c42e0b701cb5abe6b52f54b523c348fe15
priyobrato.das <priyodas12@gmail.com>
2025-09-26 01:15:49 +0530

    card:css

--- src/components/restaurants/Restaurants.jsx
+++ src/components/restaurants/Restaurants.jsx
@@ -1,18 +1,41 @@
 import React from "react";
 import './Restaurants.css'
 import RestaurantCard from "./cards/RestaurantCard";
+import { faker } from "@faker-js/faker";
 
 
+function Restaurants() {
 
-function Restaurants(){
     return <div id="restaurants">
-       <RestaurantCard></RestaurantCard>
-       <RestaurantCard></RestaurantCard>
-       <RestaurantCard></RestaurantCard>
-       <RestaurantCard></RestaurantCard>
-           <RestaurantCard></RestaurantCard>
-       <RestaurantCard></RestaurantCard>
+        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
+        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
+        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
+        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
+        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
+        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
+        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
+        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
+        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
+        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
     </div>
+}
+
+
+function getImageUrl() {
+    let images = [
+
+    ];
+    return images[Math.floor(Math.random() * images.length)]
+}
+
+function getRestaurantData() {
+    return {
+        restName: faker.company.name(),
+        restAddress: faker.location.streetAddress(),
+        cuisine: faker.food.ethnicCategory(),
+        imageUrl: getImageUrl(),
+        isOpen: faker.number.int(1, 10) < 8
+    };
 }
 
 export default Restaurants;
--- src/components/restaurants/cards/RestaurantCard.jsx
+++ src/components/restaurants/cards/RestaurantCard.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import './RestaurantCard.css';
-import uuid4 from "uuid4";
-import image1 from "../../../public/images/1.jpg"
 
-function RestaurantCard(){
-    let restId=uuid4();
-    return <div id='restaurant-card' style={{ "--card-background-image": `url(${image1})`}} >
-        <p>{restId}</p>
+
+function RestaurantCard({restData}) {
+    const { restName, restAddress, imageUrl, cuisine, isOpen } = restData;
+    return <div className="card">
+        <div className="card-image">
+            <img src={imageUrl} alt={restName} />
+        </div>
+
+        <div className="card-text">
+            <h3>{restName}</h3>
+            <p>{cuisine}</p>
+            <p>{isOpen ? "Open" : "Closed"}</p>
+            <p>{restAddress}</p>
+        </div>
     </div>
 }
 
