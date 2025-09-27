39882799ab1bed1463f2e8c66a0bece0a5a6cb24
priyobrato.das <priyodas12@gmail.com>
2025-09-28 00:58:50 +0530

    backend- connect

--- src/components/restaurants/Restaurants.jsx
+++ src/components/restaurants/Restaurants.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './Restaurants.css'
 import RestaurantCard from "./cards/RestaurantCard";
 import { faker } from "@faker-js/faker";
@@ -6,55 +6,20 @@ import { faker } from "@faker-js/faker";
 
 function Restaurants() {
 
+    const [restaurantData, setRestaurantData] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3001/api/v1/restaurants/")
+            .then(res => res.json())
+            .then((data) => setRestaurantData(data))
+            .catch((error) => console.log("error while /restaurants", error))
+    }, []);
+
     return <div id="restaurants">
-        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
-        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
-        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
-        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
-        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
-        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
-        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
-        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
-        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
-        <RestaurantCard restData={getRestaurantData()}></RestaurantCard>
+        {restaurantData.map(restData => <RestaurantCard restData={restData} key={restData.name}></RestaurantCard>)}
     </div>
 }
 
-//todo: removed while actual API call
-function getImageUrl() {
-    let images = [
 
-        'https://images.pexels.com/photos/209540/pexels-photo-209540.jpeg',
-        'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg',
-        'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg',
-        'https://images.pexels.com/photos/2983101/pexels-photo-2983101.jpeg',
-        'https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg',
-        'https://images.pexels.com/photos/1437267/pexels-photo-1437267.jpeg',
-        'https://images.pexels.com/photos/1059943/pexels-photo-1059943.jpeg',
-        'https://images.pexels.com/photos/262959/pexels-photo-262959.jpeg',
-        'https://images.pexels.com/photos/718742/pexels-photo-718742.jpeg',
-        'https://images.pexels.com/photos/2569760/pexels-photo-2569760.jpeg',
-        'https://images.pexels.com/photos/769969/pexels-photo-769969.jpeg',
-        'https://images.pexels.com/photos/691114/pexels-photo-691114.jpeg',
-        'https://images.pexels.com/photos/291528/pexels-photo-291528.jpeg',
-        'https://images.pexels.com/photos/2474658/pexels-photo-2474658.jpeg',
-        'https://images.pexels.com/photos/1051399/pexels-photo-1051399.jpeg',
-        'https://images.pexels.com/photos/913136/pexels-photo-913136.jpeg',
-        'https://images.pexels.com/photos/262897/pexels-photo-262897.jpeg'
-    ];
-    return images[Math.floor(Math.random() * images.length)]
-}
-
-//todo: removed while actual API call
-function getRestaurantData() {
-    return {
-        restName: faker.company.name(),
-        restAddress: faker.location.streetAddress(),
-        cuisine: [faker.food.ethnicCategory(), faker.food.ethnicCategory(), faker.food.ethnicCategory()],
-        imageUrl: getImageUrl(),
-        isOpen: faker.number.int(3, 10) < 8,
-        rating: Math.round(Math.random() * 9)
-    };
-}
 
 export default Restaurants;
--- src/components/restaurants/cards/RestaurantCard.jsx
+++ src/components/restaurants/cards/RestaurantCard.jsx
@@ -3,18 +3,20 @@ import './RestaurantCard.css';
 
 
 function RestaurantCard({ restData }) {
-    const { restName, restAddress, imageUrl, cuisine, isOpen, rating } = restData;
-    let cuisineList = cuisine.join(",")
+    const { name, location, imageUrl, cuisine, isOpen, rating, description } = restData;
+
     return <div className="card">
         <div className="card-image">
-            <img src={imageUrl} alt={restName} />
+            <img src={imageUrl} alt={name} />
         </div>
 
         <div className="card-text">
-            <h3>{restName}</h3>
-            <p>Special: {cuisineList}</p>
+            <h3>{name}</h3>
+            <hr />
+            <p>Description: {description}</p>
+            <p>Special: {cuisine}</p>
             <p>Ratings: {rating}</p>
-            <p>Location: {restAddress}</p>
+            <p>Location: {location}</p>
             <p>{isOpen ? "Open" : "Closed"}</p>
         </div>
     </div>
