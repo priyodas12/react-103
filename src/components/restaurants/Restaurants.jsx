0a54e2a79c9412392e310ba3c34d5b8816d3e737
priyobrato.das <priyodas12@gmail.com>
2025-09-28 12:21:04 +0530

    css: cards

--- src/components/restaurants/Restaurants.jsx
+++ src/components/restaurants/Restaurants.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import './Restaurants.css'
 import RestaurantCard from "./cards/RestaurantCard";
-import { faker } from "@faker-js/faker";
+
 
 
 function Restaurants() {
--- src/components/restaurants/cards/RestaurantCard.jsx
+++ src/components/restaurants/cards/RestaurantCard.jsx
@@ -13,11 +13,11 @@ function RestaurantCard({ restData }) {
         <div className="card-text">
             <h3>{name}</h3>
             <hr />
-            <p>Description: {description}</p>
-            <p>Special: {cuisine}</p>
-            <p>Ratings: {rating}</p>
-            <p>Location: {location}</p>
-            <p>{isOpen ? "Open" : "Closed"}</p>
+            <p><label className="card-text-label">Description:</label> {description}</p>
+            <p><label className="card-text-label">Special:</label> {cuisine}</p>
+            <p><label className="card-text-label">Ratings:</label> {rating}</p>
+            <p><label className="card-text-label">Location:</label> {location}</p>
+            <p><label className="card-text-label">Status:</label>{isOpen ? "Open" : "Closed"}</p>
         </div>
     </div>
 }
