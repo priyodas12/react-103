0fc16b504948f879c65485915c5e75e8389fd1fb
priyobrato.das <priyodas12@gmail.com>
2025-09-28 16:46:31 +0530

    css: refactor

--- src/components/restaurants/Restaurants.jsx
+++ src/components/restaurants/Restaurants.jsx
@@ -12,11 +12,15 @@ function Restaurants() {
         fetch("http://localhost:3001/api/v1/restaurants/")
             .then(res => res.json())
             .then((data) => setRestaurantData(data))
-            .catch((error) => console.log("error while /restaurants", error))
+            .catch((error) => console.log("error while fetching data from /api/v1/restaurants", error))
     }, []);
 
     return <div id="restaurants">
-        {restaurantData.map(restData => <RestaurantCard restData={restData} key={restData.name}></RestaurantCard>)}
+
+        {restaurantData.sort((rest1, rest2) => rest1.location.localeCompare(rest2.location)).map(restData =>
+            <RestaurantCard restData={restData} key={restData._id}></RestaurantCard>
+        )
+        }
     </div>
 }
 
--- src/components/restaurants/cards/RestaurantCard.jsx
+++ src/components/restaurants/cards/RestaurantCard.jsx
@@ -3,18 +3,19 @@ import './RestaurantCard.css';
 
 
 function RestaurantCard({ restData }) {
-    const { name, location, imageUrl, cuisine, isOpen, rating, description } = restData;
+    const { _id, name, offer, location, imageUrl, cuisine, isOpen, rating, description } = restData;
 
     return <div className="card">
         <div className="card-image">
-            <img src={imageUrl} alt={name} />
+            <img src={imageUrl} alt={`${_id}_${name}`} />
         </div>
 
         <div className="card-text">
-            <h3>{name}</h3>
+            <h3 id="rest-name">{name}</h3>
             <hr />
             <p><label className="card-text-label">Description:</label> {description}</p>
             <p><label className="card-text-label">Special:</label> {cuisine}</p>
+            <p><label className="card-text-label">Offer:</label> {offer}</p>
             <p><label className="card-text-label">Ratings:</label> {rating}</p>
             <p><label className="card-text-label">Location:</label> {location}</p>
             <p><label className="card-text-label">Status:</label>{isOpen ? "Open" : "Closed"}</p>
